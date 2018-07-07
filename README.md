19c6e7f91f7c5862011a237e841fd9e6b7bdf4ce
Alan Fitzpatrick <alajfit@gmail.com>
2018-07-07 20:30:07 +0100

    Reading files of the current working directory

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # vue-run
 Run Vue Single File Components from the CLI
+
+[![npm](https://img.shields.io/npm/v/vue-run.svg) ![npm](https://img.shields.io/npm/dm/vue-run.svg)](https://www.npmjs.com/package/vue-run)
+
+> Currently a WIP, star us and key an eye out
+
+<p align="center">
+    <img width="200" height="200" src="./docs/assets/vue-run.png" />
+</p>
+<p align="center">
+    <a href="https://travis-ci.org/alajfit/vue-run">
+        <img src="https://travis-ci.org/alajfit/vue-run.svg?branch=master" alt="Build Status" />
+    </a>
+</p>
+
+## Install
+
+```
+npm install vue-run -g
+```
+
+## Usage
+- When within a directory with a Single File Component
+
+```
+vsr
+```
+
+## Features
+- Run Single File Component from a directory
+- Add props on the fly to a running component
+
+## Feedback
+- Please raise any issues you find
+- This is still a work in progress and I'll update as and when I have free time
+
+<p>
+  <a href="https://www.patreon.com/staratarms" target="_blank">
+    <h3>If I've helped you out in any way why not: :blush:</h3>
+    <img src="./docs/assets/patreon.png" alt="Become a Patreon">
+  </a>
+</p>
+
+
+---
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ tests/demo/simple-component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>A Simple Component</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in magna urna. Ut id dolor non eros vestibulum rutrum eget eu purus. Vestibulum et eros massa. In hendrerit sem vel quam blandit aliquam. Nunc mauris leo, sagittis at enim in, dictum ultrices elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin rutrum nisi in tellus semper sagittis vel ac nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ullamcorper neque vel leo venenatis vestibulum.</p>
+        <p>Nunc tristique, purus in accumsan feugiat, neque sapien varius odio, non porta sapien orci sed nunc. Donec sed magna lorem. Aliquam volutpat massa quam, porttitor ornare dui iaculis et. Curabitur fermentum, dolor eu laoreet volutpat, velit nulla hendrerit dui, sed consectetur magna dui eget libero. Nulla ut egestas sapien. Duis pellentesque neque vitae ornare dignissim. Curabitur viverra velit a sem scelerisque, vitae mattis risus egestas. Integer vehicula vulputate est, at scelerisque lorem laoreet et. Pellentesque mattis tellus ex. Donec efficitur risus eu tellus pellentesque malesuada. Pellentesque finibus diam vel iaculis fringilla. Curabitur porttitor metus non quam mollis, non semper elit vulputate. Phasellus id sem tempor, lobortis est sed, elementum tortor. Nunc commodo magna at orci fringilla porttitor.</p>
+        <p>Sed quis dapibus lorem. Praesent hendrerit imperdiet molestie. Sed eu metus vel tellus varius lobortis sed id leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec gravida nulla at justo ultricies interdum. In eu aliquam tortor. Morbi a nibh id nisl laoreet luctus.</p>
+        <p>Quisque facilisis tellus nisl, vitae imperdiet nunc consectetur a. In vel fermentum tortor. In eu leo gravida, semper ipsum vitae, convallis felis. Integer et mi pulvinar, sagittis tortor nec, finibus justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi nibh lectus, rhoncus vitae tempus vitae, consequat vel tellus. In in neque eros. Nullam consectetur accumsan hendrerit. Aliquam finibus magna a facilisis iaculis. Vestibulum faucibus ex et turpis blandit rutrum. Aenean nec nulla augue.</p>
+        <p>Sed facilisis hendrerit tristique. Nulla erat nisl, viverra id rutrum id, ornare vel ex. Praesent elementum ligula malesuada enim congue, id euismod quam porta. In augue nisl, rutrum vitae hendrerit id, cursus sed nisi. Fusce id est vel nunc consectetur ornare id sit amet lacus. Morbi tempor velit quis augue molestie, nec placerat nibh consequat. Praesent risus ex, suscipit sit amet laoreet quis, congue vitae felis. Quisque lobortis, ligula id tincidunt gravida, dui orci euismod nunc, vitae commodo lacus augue varius justo.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'simple-component'
+    }
+</script>
+
